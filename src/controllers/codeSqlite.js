91da7b81f89c9b092fe43d31c3425bedcd366b26
elguesabal <113626409+elguesabal@users.noreply.github.com>
2023-05-26 21:21:15 -0300

    Add files via upload

--- src/controllers/codeSqlite.js
+++ src/controllers/codeSqlite.js
@@ -2,14 +2,28 @@ import { sqliteConnection } from "../infra/db.js";
 
 export async function saveCode(email, code) {
   sqliteConnection().then((db) =>
-    db.run(`INSERT INTO code (email, code) VALUES ('${email}', ${code});`)
+    db.run(`INSERT INTO code (email, code) VALUES ('${email}', ${code});`,
+    function (err, results) {
+        if (err) {
+          throw new Error({ message: "Error in the database" });
+        } else {
+          // console.log("terminado");
+        }
+      })
   );
 }
 
 export async function deleteCode(email, code) {
   setTimeout(function () {
     sqliteConnection().then((db) =>
-      db.run(`DELETE FROM code WHERE email = '${email}' AND code = ${code};`)
+      db.run(`DELETE FROM code WHERE email = '${email}' AND code = ${code};`,
+      function (err, results) {
+        if (err) {
+          throw new Error({ message: "Error in the database" });
+        } else {
+          // console.log("terminado");
+        }
+      })
     );
   }, 300000);
 }
