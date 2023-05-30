a5aa3ca71e25900da23715d6865fb1f83a284a70
Juanpi <juan.urgelles92@gmail.com>
2023-05-30 17:09:06 -0300

    Route Review

--- src/controllers/codeSqlite.js
+++ src/controllers/codeSqlite.js
@@ -1,20 +1,38 @@
 import { sqliteConnection } from "../infra/db.js";
 
 // 4 digit random code generator
-export async function randomCodeGenerator() {
+export function randomCodeGenerator() {
   return Math.floor(Math.random() * 9000) + 1000;
 }
 
 export async function saveEmailCode(email, code) {
   sqliteConnection().then((db) =>
-    db.run(`INSERT INTO db_emailcode (email, code) VALUES ('${email}', ${code});`)
+    db.run(
+      `INSERT INTO db_emailcode (email, code) VALUES ('${email}', ${code});`,
+      function (err, results) {
+        if (err) {
+          throw new Error({ message: "Error in the database" });
+        } else {
+          console.log("terminado", results);
+        }
+      }
+    )
   );
 }
 
 export async function deleteEmailCode(email, code) {
   setTimeout(function () {
     sqliteConnection().then((db) =>
-      db.run(`DELETE FROM db_emailcode WHERE email = '${email}' AND code = ${code};`)
+      db.run(
+        `DELETE FROM db_emailcode WHERE email = '${email}' AND code = ${code};`,
+        function (err, results) {
+          if (err) {
+            throw new Error({ message: "Error in the database" });
+          } else {
+            console.log("terminado", results);
+          }
+        }
+      )
     );
   }, 300000);
 }
@@ -36,20 +54,36 @@ export async function checkEmailCode(email, code) {
 
 //////// SMS (TEMPORARIO, A IDEIA É TER APENAS 1 FUNCAO PRA CADA AÇÃO, TANTO PRA EMAIL TANTO PRA SMS)
 
-
 export async function saveSmsCode(phone, code) {
   sqliteConnection().then((db) =>
-    db.run(`INSERT INTO db_smscode (phone, code) VALUES ('${phone}', ${code});`)
+    db.run(
+      `INSERT INTO db_smscode (phone, code) VALUES ('${phone}', ${code});`,
+      function (err, results) {
+        if (err) {
+          throw new Error({ message: "Error in the database" });
+        } else {
+          console.log("terminado", results);
+        }
+      }
+    )
   );
 }
 
 export async function deleteSmsCode(phone, code) {
   setTimeout(function () {
     sqliteConnection().then((db) =>
-      db.run(`DELETE FROM db_smscode WHERE phone = '${phone}' AND code = ${code};`)
+      db.run(
+        `DELETE FROM db_smscode WHERE phone = '${phone}' AND code = ${code};`,
+        function (err, results) {
+          if (err) {
+            throw new Error({ message: "Error in the database" });
+          } else {
+            console.log("terminado", results);
+          }
+        }
+      )
     );
   }, 300000);
-  
 }
 
 export async function checkSmsCode(phone, code) {
